f04ef1474319fbcfdd9f0f5ea62aa40a1fb70d52
ISextra <ivan2001solovei@mail.ru>
2022-09-08 18:23:57 +0300

    add interface; add button-numbers handler; add button-deleteLastSymbol handler

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
+let resultElement = document.querySelector('.top-result');
+let resultNumberOfElement = resultElement.innerHTML;
 
+const insertNumber = (number) => {
+    resultElement = document.querySelector('.top-result');
+    resultNumberOfElement = resultElement.innerHTML;
+
+    if (resultNumberOfElement.length > 16){     //ограничение по длинне числа
+        resultElement.innerHTML = `${resultNumberOfElement}`;
+    } else if (resultNumberOfElement === `0`) {     //не впысываем 0 спереди
+        resultElement.innerHTML = `${number}`;
+    } else {
+        resultElement.innerHTML = `${resultNumberOfElement}${number}`;
+    }
+
+    if (resultNumberOfElement.length > 10) {
+        resultElement.style.fontSize = "26px";
+    }
+};
+
+const deleteLastSymbol = () => {
+    resultElement = document.querySelector('.top-result');
+    resultNumberOfElement = resultElement.innerHTML;
+
+    resultElement.innerHTML = resultNumberOfElement.slice(0, -1);
+    resultNumberOfElement = resultElement.innerHTML;
+
+    if (resultNumberOfElement === ``) {
+        resultElement.innerHTML = '0';
+    }
+};
