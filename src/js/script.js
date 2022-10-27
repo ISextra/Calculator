8e7020efb72f0428f58c73a92def993cf634f941
ISextra <sabinixkensey@gmail.com>
2022-10-27 11:14:27 +0300

    style code fix

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ class CalculatorInterface {
         const objectsMassive = [`%`,`CE`,`C`,`<`,`1/x`,`x`,`x^2`,`\u221Ax`,`\xF7`,`7`,`8`,`9`,`\xD7`,`4`,`5`,`6`,`-`,`1`,`2`,`3`,`+`,`+/-`,`0`,`.`,`=`]
     }
 
-    extensionOfElement (elementName,elementContent,appendTarget,...classList) {
+    extensionOfElement (elementName, elementContent, appendTarget, ...classList) {
 
         classList.forEach(element => {
             elementName.classList.add(element);
@@ -19,8 +19,6 @@ class CalculatorInterface {
         this.calculatorHTMLClass = calculatorHTMLClass;
 
         const mainElement = document.querySelector(this.calculatorHTMLClass);
-        const supFlipNumber = document.createElement("sup");
-        const supSquaringNumber = document.createElement("sup");
 
         const topHtml = document.createElement("div");
         topHtml.classList.add("top");
@@ -44,76 +42,76 @@ class CalculatorInterface {
         topElement.append(this.topResult);
 
         const bottomPresent = document.createElement("button");
-        this.extensionOfElement(bottomPresent,`%`,bottomElement,`bottom-button`,`bottom-present`);
+        this.extensionOfElement(bottomPresent, `%`, bottomElement, `bottom-button`, `bottom-present`);
 
         const bottomDeleteLastNumber = document.createElement("button");
-        this.extensionOfElement(bottomDeleteLastNumber,`CE`,bottomElement,`bottom-button`,`delete-buttons`,`bottom-deleteLastNumber`);
+        this.extensionOfElement(bottomDeleteLastNumber, `CE`, bottomElement, `bottom-button`, `delete-buttons`, `bottom-deleteLastNumber`);
 
         const bottomDeleteAll = document.createElement("button");
-        this.extensionOfElement(bottomDeleteAll,`C`,bottomElement,`bottom-button`,`delete-buttons`,`bottom-deleteAll`);
+        this.extensionOfElement(bottomDeleteAll, `C`, bottomElement, `bottom-button`, `delete-buttons`, `bottom-deleteAll`);
 
         const bottomDeleteLast = document.createElement("button");
-        this.extensionOfElement(bottomDeleteLast,`<`,bottomElement,`bottom-button`,`delete-buttons`,`bottom-deleteLast`);
+        this.extensionOfElement(bottomDeleteLast, `<`, bottomElement, `bottom-button`, `delete-buttons`, `bottom-deleteLast`);
 
         const bottom_flipNumber = document.createElement("button");
-        this.extensionOfElement(bottom_flipNumber,`1/x`,bottomElement,`bottom-button`,`bottom-flipNumber`);
+        this.extensionOfElement(bottom_flipNumber, `1/x`, bottomElement, `bottom-button`, `bottom-flipNumber`);
 
         const bottomSquaringNumber = document.createElement("button");
-        this.extensionOfElement(bottomSquaringNumber,`x^2`,bottomElement,`bottom-button`,`bottom-squaringNumber`);
+        this.extensionOfElement(bottomSquaringNumber, `x^2`, bottomElement, `bottom-button`, `bottom-squaringNumber`);
 
         const bottomSquareRoot = document.createElement("button");
-        this.extensionOfElement(bottomSquareRoot,`\u221Ax`,bottomElement,`bottom-button`,`bottom-deleteAll`);
+        this.extensionOfElement(bottomSquareRoot, `\u221Ax`, bottomElement, `bottom-button`, `bottom-deleteAll`);
 
         const bottomDivide = document.createElement("button");
-        this.extensionOfElement(bottomDivide,`\xF7`,bottomElement,`bottom-button`,`operation-button`,`bottom-divide`);
+        this.extensionOfElement(bottomDivide, `\xF7`, bottomElement, `bottom-button`, `operation-button`, `bottom-divide`);
 
         const bottomSeven = document.createElement("button");
-        this.extensionOfElement(bottomSeven,`7`,bottomElement,`bottom-button`,`number-button`,`bottom-seven`);
+        this.extensionOfElement(bottomSeven, `7`, bottomElement, `bottom-button`, `number-button`, `bottom-seven`);
 
         const bottomEight = document.createElement("button");
-        this.extensionOfElement(bottomEight,`8`,bottomElement,`bottom-button`,`number-button`,`bottom-eight`);
+        this.extensionOfElement(bottomEight, `8`, bottomElement, `bottom-button`, `number-button`, `bottom-eight`);
 
         const bottomNine = document.createElement("button");
-        this.extensionOfElement(bottomNine,`9`,bottomElement,`bottom-button`,`number-button`,`bottom-nine`);
+        this.extensionOfElement(bottomNine, `9`, bottomElement, `bottom-button`, `number-button`, `bottom-nine`);
 
         const bottomTimes = document.createElement("button");
-        this.extensionOfElement(bottomTimes,`\xD7`,bottomElement,`bottom-button`,`operation-button`,`bottom-times`);
+        this.extensionOfElement(bottomTimes, `\xD7`, bottomElement, `bottom-button`, `operation-button`, `bottom-times`);
 
         const bottomFour = document.createElement("button");
-        this.extensionOfElement(bottomFour,`4`,bottomElement,`bottom-button`,`number-button`,`bottom-four`);
+        this.extensionOfElement(bottomFour, `4`, bottomElement, `bottom-button`, `number-button`, `bottom-four`);
 
         const bottomFive = document.createElement("button");
-        this.extensionOfElement(bottomFive,`5`,bottomElement,`bottom-button`,`number-button`,`bottom-five`);
+        this.extensionOfElement(bottomFive, `5`, bottomElement, `bottom-button`, `number-button`, `bottom-five`);
 
         const bottomSix = document.createElement("button");
-        this.extensionOfElement(bottomSix,`6`,bottomElement,`bottom-button`,`number-button`,`bottom-six`);
+        this.extensionOfElement(bottomSix, `6`, bottomElement, `bottom-button`, `number-button`, `bottom-six`);
 
         const bottomMinus = document.createElement("button");
-        this.extensionOfElement(bottomMinus,`-`,bottomElement,`bottom-button`,`operation-button`,`bottom-minus`);
+        this.extensionOfElement(bottomMinus, `-`, bottomElement, `bottom-button`, `operation-button`, `bottom-minus`);
 
         const bottomOne = document.createElement("button");
-        this.extensionOfElement(bottomOne,`1`,bottomElement,`bottom-button`,`number-button`,`bottom-one`);
+        this.extensionOfElement(bottomOne, `1`, bottomElement, `bottom-button`, `number-button`, `bottom-one`);
 
         const bottomTwo = document.createElement("button");
-        this.extensionOfElement(bottomTwo,`2`,bottomElement,`bottom-button`,`number-button`,`bottom-two`);
+        this.extensionOfElement(bottomTwo, `2`, bottomElement, `bottom-button`, `number-button`, `bottom-two`);
 
         const bottomThree = document.createElement("button");
-        this.extensionOfElement(bottomThree,`3`,bottomElement,`bottom-button`,`number-button`,`bottom-three`);
+        this.extensionOfElement(bottomThree, `3`, bottomElement, `bottom-button`, `number-button`, `bottom-three`);
 
         const bottomPlus = document.createElement("button");
-        this.extensionOfElement(bottomPlus,`+`,bottomElement,`bottom-button`,`operation-button`,`bottom-plus`);
+        this.extensionOfElement(bottomPlus, `+`, bottomElement, `bottom-button`, `operation-button`, `bottom-plus`);
 
         const bottomNegative = document.createElement("button");
-        this.extensionOfElement(bottomNegative,`+/-`,bottomElement,`bottom-button`,`number-button`,`bottom-negative`);
+        this.extensionOfElement(bottomNegative, `+/-`, bottomElement, `bottom-button`, `number-button`, `bottom-negative`);
 
         const bottomZero = document.createElement("button");
-        this.extensionOfElement(bottomZero,`0`,bottomElement,`bottom-button`,`number-button`,`bottom-zero`);
+        this.extensionOfElement(bottomZero, `0`, bottomElement, `bottom-button`, `number-button`, `bottom-zero`);
 
         const bottomPoint = document.createElement("button");
-        this.extensionOfElement(bottomPoint,`.`,bottomElement,`bottom-button`,`number-button`,`bottom-point`);
+        this.extensionOfElement(bottomPoint, `.`, bottomElement, `bottom-button`, `number-button`, `bottom-point`);
 
         const bottomResult = document.createElement("button");
-        this.extensionOfElement(bottomResult,`=`,bottomElement,`bottom-button`,`red-button`,`bottom-result`);
+        this.extensionOfElement(bottomResult, `=`, bottomElement, `bottom-button`, `red-button`,` bottom-result`);
     }
 
 }
@@ -124,8 +122,6 @@ class CalculatorNumbers extends CalculatorInterface{
 
         this.additionEventForNumbers();
     }
-
-
 
     additionEventForNumbers() {
         const topResult = this.topResult;
@@ -219,6 +215,5 @@ class CalculatorAction extends CalculatorNumbers {
         }
     }
 }
-
 
 const interface1 = new CalculatorNumbers(".calculator");
