d3a587b399bf7272be749669cc994db1e375d6ca
ISextra <sabinixkensey@gmail.com>
2023-02-13 11:06:53 +0300

    [CALCULATOR-33] Не допускать пересечения границ окна при перетаскивании калькулятора

--- src/js/dragAndDrop.js
+++ src/js/dragAndDrop.js
@@ -1,24 +1,68 @@
-import {ELEMENTS_PROPERTY} from "./constants.js"
+import {DEFAULT_VALUES} from "./constants.js"
 
 export default class DragAndDrop {
     constructor(elementForDrugAndDrop) {
-        //this.setDragendEventListener = this.setDragendEventListener.bind(this);
         this.setDragendEventListener(elementForDrugAndDrop);
     }
 
     setDragendEventListener(elementForDrugAndDrop) {
-        elementForDrugAndDrop.draggable = true;
-        let offsetX = null;
-        let offsetY = null;
+        let shiftX = null;
+        let shiftY = null;
+        let rightEdge = null;
+        let bottomEdge = null;
+        let finalPositionX = null;
+        let finalPositionY = null;
+        let documentHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+        let documentWidth = Math.max(
+            document.body.scrollWidth, document.documentElement.scrollWidth,
+            document.body.offsetWidth, document.documentElement.offsetWidth,
+            document.body.clientWidth, document.documentElement.clientWidth
+        );
 
-        elementForDrugAndDrop.addEventListener("dragstart", function(event) {
-            offsetX = event.offsetX;
-            offsetY = event.offsetY;
-        });
+        //удаление встроенного dnd
+        elementForDrugAndDrop.ondragstart = function() {
+            return false;
+        };
 
-        elementForDrugAndDrop.addEventListener("dragend", function(event) {
-            elementForDrugAndDrop.style.top = (event.pageY - offsetY) + 'px';
-            elementForDrugAndDrop.style.left = (event.pageX - offsetX) + 'px';
-        });
+        elementForDrugAndDrop.addEventListener("mousedown", function onMouseDown(event) {
+            shiftX = event.clientX - elementForDrugAndDrop.getBoundingClientRect().left;
+            shiftY = event.clientY - elementForDrugAndDrop.getBoundingClientRect().top;
+
+            function onMouseMove(event) {
+                rightEdge = documentWidth - elementForDrugAndDrop.offsetWidth;
+                bottomEdge = documentHeight - elementForDrugAndDrop.offsetHeight;
+                finalPositionX = event.pageX - shiftX;
+                finalPositionY = event.pageY - shiftY;
+                
+                if (finalPositionX < DEFAULT_VALUES.ZERO) {
+                    finalPositionX = DEFAULT_VALUES.ZERO;
+                }
+
+                if (finalPositionX > rightEdge ) {
+                    finalPositionX = rightEdge;
+                }
+
+                if (finalPositionY < DEFAULT_VALUES.ZERO) {
+                    finalPositionY = DEFAULT_VALUES.ZERO;
+                }
+
+                if (finalPositionY > bottomEdge ) {
+                    finalPositionY = bottomEdge;
+                }
+                
+                elementForDrugAndDrop.style.left = finalPositionX + 'px';
+                elementForDrugAndDrop.style.top = finalPositionY + 'px';
+            }
+
+            document.addEventListener('mousemove', onMouseMove);
+
+            elementForDrugAndDrop.addEventListener('mouseup', function(event) {
+                document.removeEventListener('mousemove', onMouseMove);
+            });
+        })
     }
 }
